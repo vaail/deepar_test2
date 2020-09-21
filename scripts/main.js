1286882de4a0b39c7928a6e338f72ee93e92bd84
Yury Kouzma <y.kouzma@sdventures.com>
2020-09-21 16:13:34 +0300

    f

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,9 @@ sourceVideo.style.top = 0
 sourceVideo.style.left = 0
 sourceVideo.style.width = '100%'
 sourceVideo.style.height = '100%'
+sourceVideo.setAttribute('autoplay', '');
+sourceVideo.setAttribute('muted', '');
+sourceVideo.setAttribute('playsinline', '');
 window.document.body.appendChild(sourceVideo)
 
 if (navigator.mediaDevices.getUserMedia) {
